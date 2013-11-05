c46c754cb1425d45537ceaec02296b402560c84f
Adrian Tan <atks@umich.edu>
2013-11-05 10:31:23 -0500

    added genome_interval.*

---
+++ genome_interval.cpp
@@ -0,0 +1,66 @@
+/* The MIT License
+
+   Copyright (c) 2013 Adrian Tan <atks@umich.edu>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#include "genome_interval.h"
+
+GenomeInterval::GenomeInterval(std::string interval)
+{
+    std::cerr << "interval: " << interval << "\n";
+    
+    std::vector<std::string> v;
+    split(v, ":-", interval);
+    
+    if (v.size()==1)
+    {
+        seq = v[0];
+        start1 = 1; 
+        end1 = (1<<29) - 1;
+    }
+    else if (v.size()==3)
+    {
+        seq = v[0];
+        if (!str2int32(v[1], start1) || !str2int32(v[2], end1))
+        {
+            fprintf(stderr, "[%s:%d %s] Invalid genomic interval: %s\n", __FILE__,__LINE__,__FUNCTION__, interval.c_str());
+            exit(1);
+        }
+    }
+    else
+    {
+        fprintf(stderr, "[%s:%d %s] Invalid genomic interval: %s\n", __FILE__,__LINE__,__FUNCTION__, interval.c_str());
+        exit(1);
+    }
+    
+    std::cerr << "interval       : " << interval << "\n";
+    std::cerr << "parsed interval: " << seq << ":" << start1 << "-" << end1 << "\n";    
+};
+
+/**
+ * Returns a string representation of this Genome Interval. 
+ */    
+std::string GenomeInterval::to_string()
+{
+    std::stringstream ss;
+    ss << seq << ":" << start1 << "-" << end1;
+    return ss.str();      
+};
---
+++ genome_interval.h
@@ -0,0 +1,62 @@
+/* The MIT License
+
+   Copyright (c) 2013 Adrian Tan <atks@umich.edu>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#ifndef GENOME_INTERVAL_H
+#define GENOME_INTERVAL_H
+
+#include <queue>
+#include <sstream>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include <float.h>
+#include <vector>
+#include <map>
+#include "hts_utils.h"
+#include "utils.h"
+
+class GenomeInterval
+{
+    public:
+    std::string seq;    
+    int32_t start1; // 1 based coordinate
+    int32_t end1;   // 1 based coordinate
+    
+    /**
+     * Constructs a Genome Interval from a string representation. 
+     *
+     * @interval    string representation of an interval.
+     *
+     * e.g X:2000-4000   position 2000 to 4000 on chromosome X
+     *     Y             the entirety of chromosome Y
+     */
+    GenomeInterval(std::string interval);
+    
+    /**
+     * Returns a string representation of this Genome Interval. 
+     */    
+    std::string to_string();
+};
+    
+#endif
