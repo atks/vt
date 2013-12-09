6b56bc302f81c6d71e613ea5260b5fd2029dbbd3
Adrian Tan <adriantks@gmail.com>
2013-12-09 10:09:05 -0500

    rewrote definitions for variant classification to accomodate for STR and per allelic variant types which seems to be the right thing to do.

--- variant_manip.h
+++ variant_manip.h
@@ -33,43 +33,94 @@
 #include <map>
 #include <queue>
 #include <list>
-#include <sstream>
+#include <string>
+#include <iostream>
 #include "htslib/faidx.h"
-#include "htslib/vcf.h"
-#include "htslib/kseq.h"
-#include "hts_utils.h"
+#include "htslib/kstring.h"
 
-#define VT_REF   0
-#define VT_SNP   1
-#define VT_MNP   2
-#define VT_INSERTION  4
-#define VT_DELETION   8
-#define VT_INDEL (VT_INSERTION|VT_DELETION)
-#define VT_CLUMP 64
-#define VT_CLUMP 64
-#define VT_COMPLEX (VT_SNP|VT_INSERTION|VT_DELETION)
+//int BitCount(unsigned int u)
+//{
+//     unsigned int uCount;
+//
+//#ifndef countbit32
+//#define countbit32(x) (y = u - ((u >> 1) & 033333333333) - ((u >> 2) & 011111111111); ((y + (y >> 3)) & 030707070707) % 63);
+//
+//#endif
 
+     
+#define VT_REF      0   //dlen==0 && diff==0
+#define VT_SNP      1   //min(rlen,alen)==1 && diff==1
+#define VT_MNP      2   //min(rlen,alen)==diff
+#define VT_INDEL    4   //diff!=0 && (rlen==1 || alen==1) 
+#define VT_COMPLEX  8   //all others
+
+  
 /**
- * Variant.
+ * Allele.
+ *
+ * Allele can be described to be a SNP or INDEL or etc. with respect to the reference
+ * type - give the variant type
+ * len  - difference in length between the ref and alt with positive inferring an insertion and negative inferring a deletion
+ * tlen - assuming variant is VNTR, tlen is the tract length of the repeated motif of the VNTR
  */
-class Variant
+class Allele
 {
 	public:
 	    
 	int32_t type;
-	int32_t len;     //variant length
-	kstring_t motif; //motif
-    int32_t mlen;    //motif length
-    int32_t tlen;    //reference tract length
-    //std::vector 
-        
+	int32_t diff;  //number of difference bases when bases are compared
+	int32_t alen;  //length(alt)
+	int32_t dlen;  //length(alt)-length(ref)
+	int32_t tlen;  //tract length
+
+    Allele(int32_t type, int32_t diff,	int32_t alen, int32_t dlen, int32_t tlen)
+    {
+        this->type = type;
+        this->diff = diff;
+        this->alen = alen;
+        this->dlen = dlen; 
+    	this->tlen = tlen;
+    }
+
+    Allele() 
+    {
+        clear();
+    };
+    
+    ~Allele() {};
+    
     void clear()
     {
+        type = -1;
+        diff = -1;
+        alen = 0;
+        dlen = 0; 
+    	tlen = 0;
+    }
+};
+
+/**
+ * Variant.
+ * Describes the variant in question.
+ */
+class Variant
+{
+	public:
+	     
+	int32_t type;    //aggegrated type from the alleles
+	int32_t rlen;    //reference length
+    kstring_t motif; //motif
+    int32_t mlen;    //motif length
+    int32_t tlen;    //reference tract length
+    std::vector<Allele> alleles; 
+
+    Variant()
+    {
         type = 0;
-        len = 0; 
-    	motif.l = 0;
+        motif = {0,0,0};
         mlen = 0;
         tlen = 0;
+        alleles.clear();
     }
     
     ~Variant()
@@ -78,37 +129,15 @@ class Variant
         {
             free(motif.s);
         }    
-    }
-};
-
-/**
- * Allele
- */
-class Allele
-{
-	public:
-	    
-	int32_t type;
-	int32_t len;  //variant length
-	kstring_t motif;  //motif
-    int32_t mlen; //motif length
-    int32_t tlen; //reference tracct length
-
+    }  
+            
     void clear()
     {
         type = 0;
-        len = 0; 
-    	motif.l = 0;
+        motif.l = 0;
         mlen = 0;
         tlen = 0;
-    }
-    
-    ~Allele()
-    {
-        if (motif.m)
-        {
-            free(motif.s);
-        }    
+        alleles.clear();
     }
 };
 
@@ -126,7 +155,7 @@ class VariantManip
     /**
      * Detects near by STRs.
      */
-    bool detect_str(const char* chrom, uint32_t pos1, std::string& motif, uint32_t& tlen);
+    bool detect_str(const char* chrom, uint32_t pos1, Variant& variant);
 
     /**
      * Converts VTYPE to string.
