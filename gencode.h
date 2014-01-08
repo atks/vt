d83bfd8abc83c7e99999194304a99854829e0b76
Adrian Tan <atks@umich.edu>
2014-01-08 14:33:37 -0500

    added gencode objects

---
+++ gencode.h
@@ -0,0 +1,25 @@
+#ifndef GENCODE_H
+#define GENCODE_H
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstring>
+#include <cmath>
+#include <cfloat>
+#include <vector>
+#include <map>
+#include <queue>
+#include <list>
+#include <string>
+#include <iostream>
+#include "htslib/faidx.h"
+#include "htslib/kstring.h"
+#include "htslib/vcf.h"
+#include "htslib/tbx.h"
+#include "hts_utils.h"
+#include "utils.h"
+#include "interval_tree.h"
+#include "variant_manip.h"
+#include "gtf.h"
+
+#endif
