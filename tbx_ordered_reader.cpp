5255f6768347a35a8080b785dd472930cefaff52
Adrian Tan <adriantks@gmail.com>
2014-01-08 14:26:04 -0500

    added tabixed ordered readers for managing index GTF files

---
+++ tbx_ordered_reader.cpp
@@ -0,0 +1,145 @@
+/* The MIT License
+
+   Copyright (c) 2013 Adrian Tan <atks@umich.edu>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#include "tbx_ordered_reader.h"
+
+TBXOrderedReader::TBXOrderedReader(std::string gz_file, std::vector<GenomeInterval>& intervals)
+{		
+    this->hts_file = hts_file;
+    this->intervals = intervals;
+    interval_index = 0;
+
+    hts = NULL;
+    tbx = NULL;
+    itr = NULL;
+
+    s = {0, 0, 0};
+    
+    hts = hts_open(hts_file.c_str(), "r");
+		
+    intervals_present =  intervals.size()!=0;
+
+    if ((tbx = tbx_index_load(gz_file.c_str())))
+    {
+        index_loaded = true;
+    }
+    else
+    {
+        if (intervals_present)
+        {
+            fprintf(stderr, "[E:%s] index cannot be loaded for %s\n", __FUNCTION__, gz_file.c_str());
+            exit(1);
+        }
+    }
+
+    random_access_enabled = intervals_present && index_loaded;
+};
+
+/**
+ * Jump to interval. Returns false if not successful.
+ *
+ * @interval - string representation of interval.
+ */
+bool TBXOrderedReader::jump_to_interval(GenomeInterval& interval)
+{
+    if (index_loaded)
+    {
+        intervals_present = true;
+        random_access_enabled = true;
+        intervals.clear();
+        intervals.push_back(interval);
+        interval_index = 0;
+       
+        intervals[interval_index++].to_string(&s);
+        itr = tbx_itr_querys(tbx, s.s);
+        if (itr)
+        {
+            return true;
+        }    
+    }
+
+    return false;
+};
+
+/**
+ * Initialize next interval.
+ * Returns false only if all intervals are accessed.
+ */
+bool TBXOrderedReader::initialize_next_interval()
+{
+    while (interval_index!=intervals.size())
+    {
+        intervals[interval_index++].to_string(&s);
+        itr = tbx_itr_querys(tbx, s.s);
+        if (itr)
+        {
+            return true;
+        }     
+    }
+
+    return false;
+};
+
+/**
+ * Reads next record, hides the random access of different regions from the user.
+ */
+bool TBXOrderedReader::read(kstring_t *s)
+{
+    if (random_access_enabled)
+    {
+        while(true)
+        {
+            if (itr && tbx_itr_next(hts, tbx, itr, s)>=0)
+            {
+                return true;
+            }
+            else if (!initialize_next_interval())
+            {
+                return false;
+            }
+        }
+    }
+    else
+    {
+        if (hts_getline(hts, '\n', s) >= 0)
+		{
+		    return true;
+		}
+		else
+	    {
+	        return false;    
+	    }
+    }
+
+    return false;
+};
+
+/**
+ * Closes the file.
+ */
+void TBXOrderedReader::close()
+{
+    hts_close(hts);
+}
+
+
---
+++ tbx_ordered_reader.h
@@ -0,0 +1,130 @@
+/* The MIT License
+
+   Copyright (c) 2014 Adrian Tan <atks@umich.edu>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#ifndef TBX_ORDERED_READER_H
+#define TBX_ORDERED_READER_H
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstring>
+#include <cmath>
+#include <cfloat>
+#include <vector>
+#include <list>
+#include <map>
+#include <queue>
+#include "htslib/vcf.h"
+#include "htslib/tbx.h"
+#include "htslib/bgzf.h"
+#include "hts_utils.h"
+#include "genome_interval.h"
+#include "interval_tree.h"
+
+/**
+ * A class for reading ordered VCF/BCF files.
+ *
+ * Basically a record iterator that hides the
+ * htslib interface from the programs in vt.
+ *
+ * The main impetus for this is that htslib
+ * is currently incorporated as a very early
+ * stage and is thus lacking many feature that
+ * is useful to us at this point in time. This
+ * allows us to isolate the changes required in
+ * future to simply methods in hts_utils.
+ *
+ * The following cases are supported.
+ *
+ * 1) Input is an unindexed file which is not necessarily ordered.
+ * 2) Input is an indexed file
+ *
+ * This class hides the handling of indices from
+ * the user and also allows for the selection of
+ * records in intervals in both cases 1 and 2.
+ */
+
+class TBXOrderedReader
+{
+    public:
+
+    ///////
+    //i/o//
+    ///////
+    std::string hts_file;
+    htsFile *hts;
+    tbx_t *tbx;
+    hts_itr_t *itr;
+
+    //for control
+    bool intervals_present;
+    bool index_loaded;
+    bool random_access_enabled;
+
+    //list of intervals
+    std::vector<GenomeInterval> intervals;
+    uint32_t interval_index;
+    
+    //shared objects for string manipulation
+    kstring_t s;
+
+    /**
+     * Initialize files and intervals.
+     *
+     * @input_vcf_file     name of the input VCF file
+     * @intervals          list of intervals, if empty, all records are selected.
+     */
+    TBXOrderedReader(std::string input_vcf_file, std::vector<GenomeInterval>& intervals);
+
+    /**
+     * Jump to interval. Returns false if not successful.
+     *
+     * @interval - string representation of interval.
+     */
+    bool jump_to_interval(GenomeInterval& interval);
+
+    /**
+     * Returns next vcf record.
+     */
+    bool read(kstring_t *s);
+
+    /**
+    * Initialize next interval.
+    * Returns false only if all intervals are accessed.
+    */
+    bool initialize_next_interval();
+
+    /**
+     * Returns next set of vcf records at a start position.
+     * Note that this function should never be used in conjunction with read(bcf1_t *v)
+     */
+    bool read_next_position(std::vector<bcf1_t *>& vs);
+
+    /**
+     * Closes the file.
+     */
+    void close();
+
+    private:
+};
+
+#endif
