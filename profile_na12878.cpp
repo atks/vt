49f8b37e066d78e9331e2a2cc6dbed54a77ba8dd
Adrian Tan <atks@umich.edu>
2014-01-30 13:32:12 -0500

    added concordance comparison for profile_na12878

--- profile_na12878.cpp
+++ profile_na12878.cpp
@@ -46,6 +46,24 @@ class OverlapStats
     };
 };
 
+class ConcordanceStats
+{
+    public:
+
+    int32_t geno[4][4];
+
+    ConcordanceStats()
+    {
+        for (int32_t i=0; i<4; ++i)
+        {
+            for (int32_t j=0; j<4; ++j)
+            {
+                geno[i][j] = 0;
+            }
+        }
+    };
+};
+
 class Igor : Program
 {
     public:
@@ -61,14 +79,12 @@ class Igor : Program
     std::string ref_data_sets_list;
     std::vector<GenomeInterval> intervals;
     std::string interval_list;
-
     std::vector<std::string> dataset_labels;
     std::vector<std::string> dataset_types;
-
     std::vector<OverlapStats> stats;
-
+    std::vector<ConcordanceStats> concordance;
     std::string gencode_gtf_file;
-
+    bool gencode_exists;
 
     ///////
     //i/o//
@@ -146,14 +162,10 @@ class Igor : Program
 //#           file is used for GENCODE annotation of frame shift and non frame shift Indels
 //# filter  - filter applied to variants for this particular data set
 //# path    - path of indexed BCF file
-//#dataset              type         filter           path
-//mills                 TP           INDEL            /net/fantasia/home/atks/dev/vt/ftp/grch37/mills.208620indels.sites.bcf
-//mills.chip            TP           INDEL            /net/fantasia/home/atks/dev/vt/ftp/grch37/mills.chip.158samples.8904indels.sites.bcf
-//mills.chip.common     TP           INDEL&&AF>0.005  /net/fantasia/home/atks/dev/vt/ftp/grch37/mills.chip.158samples.8904indels.sites.bcf
-//affy.exome.chip       TP           INDEL            /net/fantasia/home/atks/dev/vt/ftp/grch37/affy.exome.chip.1249samples.316520variants.sites.bcf
-//affy.exome.chip.poly  TP           INDEL&&AC!=0     /net/fantasia/home/atks/dev/vt/ftp/grch37/affy.exome.chip.1249samples.316520variants.sites.bcf
-//affy.exome.chip.mono  FP           INDEL&&AC=0      /net/fantasia/home/atks/dev/vt/ftp/grch37/affy.exome.chip.1249samples.316520variants.sites.bcf
-//gencode.v19           annotation   .                /net/fantasia/home/atks/dev/vt/ftp/grch37/gencode.v19.annotation.gtf.gz
+//#dataset              type         filter    path
+//broad.kb              TP           PASS      /net/fantasia/home/atks/dev/vt/bundle/public/grch37/broad.kb.241365variants.genotypes.bcf
+//illumina.platinum     TP           PASS      /net/fantasia/home/atks/dev/vt/bundle/public/grch37/NA12878.illumina.platinum.5284448variants.genotypes.bcf
+//gencode.v19           annotation   .         /net/fantasia/home/atks/dev/vt/bundle/public/grch37/gencode.v19.annotation.gtf.gz
 
         input_vcf_files.push_back(input_vcf_file);
         dataset_labels.push_back("data");
@@ -198,7 +210,12 @@ class Igor : Program
         //tool initialization//
         ///////////////////////
         vm = new VariantManip(ref_fasta_file);
-        gc = new GENCODE(gencode_gtf_file, ref_fasta_file);
+        gencode_exists = false;
+        if (gencode_gtf_file != "")
+        {
+            gencode_exists = true;
+            gc = new GENCODE(gencode_gtf_file, ref_fasta_file);
+        }
 
         ////////////////////////
         //stats initialization//
@@ -212,7 +229,7 @@ class Igor : Program
         common_nfs = 0;
     }
 
-    void profile_indels()
+    void profile_na12878()
     {
         //for combining the alleles
         std::vector<bcfptr*> current_recs;
@@ -220,7 +237,21 @@ class Igor : Program
         Variant variant;
         int32_t no_overlap_files = input_vcf_files.size();
         std::vector<int32_t> presence(no_overlap_files, 0);
+        std::vector<bcfptr*> presence_bcfptr(no_overlap_files, NULL);
         stats.resize(no_overlap_files);
+        concordance.resize(no_overlap_files);
+
+        int32_t *gts = NULL;
+        int32_t n = 0;
+        int32_t x1, x2, x;
+
+        int32_t na12878_index[no_overlap_files];
+
+        //get NA12878 gt position from reference file
+        for (int32_t i=0; i<no_overlap_files; ++i)
+        {
+            na12878_index[i] = bcf_hdr_id2int(sr->hdrs[i], BCF_DT_SAMPLE, "NA12878");
+        }
 
         while(sr->read_next_position(current_recs))
         {
@@ -229,7 +260,7 @@ class Igor : Program
             bcf_hdr_t *h = current_recs[0]->h;
             int32_t vtype = vm->classify_variant(h, v, variant);
 
-            if (!((vtype==VT_INDEL || vtype==(VT_SNP|VT_INDEL)) && bcf_get_n_allele(v)==2))
+            if (bcf_get_n_allele(v)==2 && !((vtype==VT_INDEL || vtype==(VT_SNP|VT_INDEL)) ))
             {
                 continue;
             }
@@ -242,39 +273,43 @@ class Igor : Program
             for (uint32_t i=0; i<current_recs.size(); ++i)
             {
                 ++presence[current_recs[i]->file_index];
+                presence_bcfptr[current_recs[i]->file_index] = current_recs[i];
             }
 
             //annotate
             if (presence[0])
             {
-                gc->search(chrom, start1+1, end1, overlaps);
-
-                bool cds_found = false;
-                bool is_fs = false;
-                    
-                for (int32_t i=0; i<overlaps.size(); ++i)
+                if (gencode_exists)
                 {
-                    GENCODERecord *rec = (GENCODERecord *) overlaps[i];
-                    if (rec->feature==GC_FT_CDS)
+                    gc->search(chrom, start1+1, end1, overlaps);
+
+                    bool cds_found = false;
+                    bool is_fs = false;
+
+                    for (int32_t i=0; i<overlaps.size(); ++i)
                     {
-                        cds_found = true;
-                        if (abs(variant.alleles[0].dlen)%3!=0)
+                        GENCODERecord *rec = (GENCODERecord *) overlaps[i];
+                        if (rec->feature==GC_FT_CDS)
                         {
-                            is_fs = true;
-                            break;
+                            cds_found = true;
+                            if (abs(variant.alleles[0].dlen)%3!=0)
+                            {
+                                is_fs = true;
+                                break;
+                            }
                         }
                     }
-                }
 
-                if (cds_found)
-                {
-                    if (is_fs)
+                    if (cds_found)
                     {
-                        ++fs;
-                    }
-                    else
-                    {
-                        ++nfs;
+                        if (is_fs)
+                        {
+                            ++fs;
+                        }
+                        else
+                        {
+                            ++nfs;
+                        }
                     }
                 }
 
@@ -289,6 +324,18 @@ class Igor : Program
                 ++stats[0].a;
                 stats[0].a_ins += ins;
                 stats[0].a_del += del;
+
+                bcf_unpack(v, BCF_UN_STR);
+                int k = bcf_get_genotypes(presence_bcfptr[0]->h, presence_bcfptr[0]->v, &gts, &n);
+                x1 = bcf_gt_allele(gts[na12878_index[0]*2]);
+                x2 = bcf_gt_allele(gts[na12878_index[0]*2+1]);
+
+                
+                x = x1+x2;
+                if (x==-2)
+                {
+                    x = 3;
+                }
             }
 
             //update overlap stats
@@ -305,6 +352,19 @@ class Igor : Program
                     ++stats[i].ab;
                     stats[i].ab_ins += ins;
                     stats[i].ab_del += del;
+
+                    bcf_unpack(presence_bcfptr[i]->v, BCF_UN_IND);
+                    int k = bcf_get_genotypes(presence_bcfptr[i]->h, presence_bcfptr[i]->v, &gts, &n);
+
+                    int32_t y1 = bcf_gt_allele(gts[na12878_index[i]*2]);
+                    int32_t y2 = bcf_gt_allele(gts[na12878_index[i]*2+1]);
+                    int32_t y = y1+y2;
+                    if (y==-2)
+                    {
+                        y = 3;
+                    }
+
+                    ++concordance[i].geno[x][y];
                 }
                 else if (!presence[0] && presence[i])
                 {
@@ -318,6 +378,7 @@ class Igor : Program
                 }
 
                 presence[i]=0;
+                presence_bcfptr[i]=NULL;
             }
 
             presence[0] = 0;
@@ -326,7 +387,7 @@ class Igor : Program
 
     void print_options()
     {
-        std::clog << "profile_indels v" << version << "\n\n";
+        std::clog << "profile_na12878 v" << version << "\n\n";
         std::clog << "\n";
         std::clog << "Options:     input VCF File                 " << input_vcf_file << "\n";
         std::clog << "         [g] reference data sets list file  " << ref_data_sets_list << "\n";
@@ -362,6 +423,41 @@ class Igor : Program
             }
             fprintf(stderr, "\n");
         }
+
+        for (int32_t i=1; i<dataset_labels.size(); ++i)
+        {
+            int32_t (&geno)[4][4] = concordance[i].geno;
+            int32_t total = 0;
+            int32_t concordance = 0;
+            for (int32_t i=0; i<3; ++i)
+            {
+                for (int32_t j=0; j<3; ++j)
+                {
+                    total += geno[i][j];
+                    if (i==j)
+                    {
+                        concordance += geno[i][j];
+                    }
+                }
+            }
+
+            int32_t discordance = total-concordance;
+
+            fprintf(stderr, "  %s\n", dataset_labels[i].c_str());
+            fprintf(stderr, "                R/R       R/A       A/A       ./.\n");
+            fprintf(stderr, "    R/R    %8d  %8d  %8d  %8d\n", geno[0][0], geno[0][1], geno[0][2], geno[0][3]);
+            fprintf(stderr, "    R/A    %8d  %8d  %8d  %8d\n", geno[1][0], geno[1][1], geno[1][2], geno[1][3]);
+            fprintf(stderr, "    A/A    %8d  %8d  %8d  %8d\n", geno[2][0], geno[2][1], geno[2][2], geno[2][3]);
+            fprintf(stderr, "    ./.    %8d  %8d  %8d  %8d\n", geno[3][0], geno[3][1], geno[3][2], geno[3][3]);
+            fprintf(stderr, "\n");
+
+            fprintf(stderr, "    Total genotype pairs :  %8d\n", total);
+            fprintf(stderr, "    Concordance          :  %5.2f%% (%d)\n", (float)concordance/total*100, concordance);
+            fprintf(stderr, "    Discordance          :  %5.2f%% (%d)\n", (float)discordance/total*100, discordance);
+
+            fprintf(stderr, "\n");
+        }
+
     };
 
     ~Igor()
@@ -378,6 +474,6 @@ void profile_na12878(int argc, char ** argv)
     Igor igor(argc, argv);
     igor.print_options();
     igor.initialize();
-    igor.profile_indels();
+    igor.profile_na12878();
     igor.print_stats();
 }
