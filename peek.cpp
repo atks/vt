0e0d0f6b8c236f24034eb098444e9fb129890089
Adrian Tan <atks@umich.edu>
2014-09-06 23:47:42 -0400

    remoevd debug flag

--- peek.cpp
+++ peek.cpp
@@ -142,7 +142,7 @@ class Igor : Program
         /////////////////////////
         //filter initialization//
         /////////////////////////
-        filter.parse(fexp.c_str(), true);
+        filter.parse(fexp.c_str(), false);
         filter_exists = fexp=="" ? false : true;
 
         ////////////////////////
