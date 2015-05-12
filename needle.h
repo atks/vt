68f5f10aaa2e1beb79a4e3f0270af2c9d67fdf55
Manuel Holtgrewe <manuel.holtgrewe@charite.de>
2015-05-12 10:57:25 +0200

    Adding option for alignment-based decompose_block.
    The algorithm works as follows:
    
    - compute pairwise edit distance alignment, using Needleman-Wunsch
      algorithm, the first bases are force-aligned
    - go over the alignment from left to right and break apart the alignment
      as follows
    - each match followed by one or more gaps and each mismatch starts a new
      chunk
    - gaps are assigned to the chunk left of them
    - matches that are not followed by any gap are removed
    
    For example
    
        ref  CGTAACT--TCTAGT
        alt  C-TACCACTCGAAGG
    
    will be split into the following alignment parts (shown separted by
    space columns of width 3 below)
    
        ref CG   A   T--   T   C   T   T
        alt C-   C   ACT   C   G   A   G
    
    This splitting is quite aggressive but needs to be done for aligners
    such as Freebayes that tends to pull together SNVs with nearby indels
    even if the indel has a very small support only.

---
+++ needle.h
@@ -0,0 +1,102 @@
+/* The MIT License
+
+   Copyright (c) 2015 Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+#ifndef NEEDLE_H
+#define NEEDLE_H
+
+#include <vector>
+
+#include "log_tool.h"
+
+class NWParameters
+{
+    public:
+
+    int score_match;
+    int score_mismatch;
+    int score_gap;
+
+    NWParameters() : score_match(0), score_mismatch(-1), score_gap(-1)
+    {}
+};
+
+class NeedlemanWunsch
+{
+    public:
+
+    // in CIGAR: "?", "D", "M", "X", "I"
+    enum Traceback { EMPTY, CIGAR_D, CIGAR_M, CIGAR_X, CIGAR_I };
+
+    bool debug;
+    const char* ref;
+    const char* read;
+
+    int len_ref;
+    int len_read;
+
+    std::vector<int> scores;
+    std::vector<Traceback> matrix;
+    std::vector<Traceback> trace;
+
+    LogTool *lt;
+
+    NWParameters params;
+
+    /**
+     * Constructor.
+     */
+    NeedlemanWunsch(bool debug=false);
+
+    /**
+     * Constructor.
+     */
+    NeedlemanWunsch(LogTool *lt, bool debug=false);
+
+    void set_read(const char * read) {
+        this->read = read;
+    }
+
+    /**
+     * Align and compute genotype likelihood.
+     */
+    void align(const char* ref, const char* read);
+
+    /**
+     * Trace path after alignment and write to trace.
+     */
+    void trace_path();
+
+    /**
+     * Prints an alignment.
+     */
+    void print_alignment()
+    {
+        print_alignment("");
+    }
+
+    /**
+     * Prints an alignment with padding.
+     */
+    void print_alignment(std::string const & pad);
+};
+
+#endif  // ifndef NEEDLE_H
