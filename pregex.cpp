0b463e1f1549523591e8518a766436e1bb6ff864
Adrian Tan <atks@umich.edu>
2015-06-18 11:20:04 -0400

    added pregex.*

---
+++ pregex.cpp
@@ -0,0 +1,108 @@
+/* The MIT License
+
+   Copyright (c) 2015 Adrian Tan <atks@umich.edu>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#include "pregex.h"
+
+/**
+ * Constructor.
+ */
+PERLregex::PERLregex()
+{
+   regex = "";
+   re = NULL;
+   match_data = NULL;
+};
+
+/**
+ * Destructor.
+ */
+PERLregex::~PERLregex()
+{
+    if (re) pcre2_code_free(re);
+    if (match_data) pcre2_match_data_free(match_data);
+};
+
+/**
+ * Sets the regular expression amd compiles it for matching later.
+ */
+void PERLregex::set(std::string& regex)
+{
+    set(const_cast<char*>(regex.c_str()));
+};
+
+/**
+ * Sets the regular expression amd compiles it for matching later.
+ */
+void PERLregex::set(char* regex)
+{
+    if (re) pcre2_code_free(re);
+    if (match_data) pcre2_match_data_free(match_data);
+
+    this->regex = regex;
+    pattern = (PCRE2_SPTR) regex;
+
+    re = pcre2_compile(
+                pattern,               /* the pattern */
+                PCRE2_ZERO_TERMINATED, /* indicates pattern is zero-terminated */
+                0,                     /* default options */
+                &errornumber,          /* for error number */
+                &erroroffset,          /* for error offset */
+                NULL);
+
+    if (re == NULL)
+    {
+        PCRE2_UCHAR buffer[256];
+        pcre2_get_error_message(errornumber, buffer, sizeof(buffer));
+        printf("PCRE2 compilation failed at offset %d: %s\n", (int) erroroffset, buffer);
+    }
+
+    match_data = pcre2_match_data_create_from_pattern(re, NULL);
+};
+
+/**
+ * Matches a text against a regular expression that has been compiled in set().
+ */
+bool PERLregex::match(std::string& text)
+{
+    return match(const_cast<char*>(text.c_str()));    
+};
+
+/**
+ * Matches a text against a regular expression that has been compiled in set().
+ */
+bool PERLregex::match(char* text)
+{
+    subject = (PCRE2_SPTR) text;
+    subject_length = strlen((char *)subject);
+
+    rc = pcre2_match(
+                re,                   /* the compiled pattern */
+                subject,              /* the subject string */
+                subject_length,       /* the length of the subject */
+                0,                    /* start at offset 0 in the subject */
+                0,                    /* default options */
+                match_data,           /* block for storing the result */
+                NULL);                /* use default match context */
+
+    return rc>0;
+};
---
+++ pregex.h
@@ -0,0 +1,93 @@
+/* The MIT License
+
+   Copyright (c) 2015 Adrian Tan <atks@umich.edu>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#ifndef PREGEX_H
+#define PREGEX_H
+
+#include "pcre2.h"
+#include <cstdlib>
+#include <cstring>
+#include <string>
+
+/**
+ * Class for PERL regular expressions.
+ */
+class PERLregex
+{
+    public:
+    std::string regex;
+    pcre2_code *re;
+    PCRE2_SPTR pattern;     /* PCRE2_SPTR is a pointer to unsigned code units of */
+    PCRE2_SPTR subject;     /* the appropriate width (8, 16, or 32 bits). */
+    PCRE2_SPTR name_table;
+
+    int crlf_is_newline;
+    int errornumber;
+    int find_all;
+    int i;
+    int namecount;
+    int name_entry_size;
+    int rc;
+    int utf8;
+
+    uint32_t option_bits;
+    uint32_t newline;
+
+    PCRE2_SIZE erroroffset;
+    PCRE2_SIZE *ovector;
+
+    size_t subject_length;
+    pcre2_match_data *match_data;
+
+    /**
+     * Constructor.
+     */
+    PERLregex();
+
+    /**
+     * Destructor.
+     */
+    ~PERLregex();
+    
+    /**
+     * Sets the regular expression amd compiles it for matching later.
+     */
+    void set(std::string& regex);
+
+    /**
+     * Sets the regular expression amd compiles it for matching later.
+     */
+    void set(char* regex);
+        
+    /**
+     * Matches a text against a regular expression that has been compiled in set().
+     */
+    bool match(std::string& text);
+        
+    /**
+     * Matches a text against a regular expression that has been compiled in set().
+     */
+    bool match(char* text);    
+};
+
+#endif
