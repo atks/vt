385666e8b7b53868eaacef47b877598d80afb226
Adrian Tan <atks@umich.edu>
2016-01-15 12:08:46 -0500

    added test/test.sh

---
+++ test/test.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# Removes the time from vt stderr output.
+strip_stderr()
+{
+    sed 's/Time elapsed.*/Time elapsed <stripped>/g' | sed 's/file.*/file <stripped>/g' 
+}
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+VT=${DIR}/../vt
+REF=${DIR}/ref/20.fa.gz
+TMPDIRS="";
+
+echo "++++++++++++++++++++++" >&2
+echo "Tests for vt normalize" >&2
+echo "++++++++++++++++++++++" >&2
+
+# ---------------------------------------------------------------------------
+# Test 01: normalization of indels from Mills et al. chromosome 20
+# ---------------------------------------------------------------------------
+
+# create temporary directory and ensure cleanup on termination
+CMDDIR=${DIR}/normalize
+TMPDIR=${CMDDIR}/tmp
+mkdir -p ${TMPDIR}
+TMPDIRS+=" $TMPDIR";
+
+echo "testing normalize"
+
+if [ "$1" == "debug" ]; then
+    set -x
+fi
+
+${OUT}${VT} \
+            normalize \
+            ${CMDDIR}/01_IN.vcf \
+            -r ${REF} \
+            -o ${TMPDIR}/01_OUT.vcf \
+            2>&1 | strip_stderr > ${TMPDIR}/01_OUT.stderr
+
+OUT=`diff ${CMDDIR}/01_OUT.vcf ${TMPDIR}/01_OUT.vcf`
+ERR=`diff ${CMDDIR}/01_OUT.stderr ${TMPDIR}/01_OUT.stderr`
+
+set +x
+
+echo -n "             output VCF file :"
+if [ "$OUT" == "" ]; then
+    echo " ok"
+else
+    echo " NOT OK!!!"
+fi
+
+echo -n "             output logs     :"
+if [ "$ERR" == "" ]; then
+    echo " ok"
+else
+    echo " NOT OK!!!"
+fi
+
+echo "+++++++++++++++++++++++++++++++" >&2
+echo "Tests for vt decompose_blocksub" >&2
+echo "+++++++++++++++++++++++++++++++" >&2
+
+# create temporary directory and ensure cleanup on termination
+CMDDIR=${DIR}/decompose_blocksub
+TMPDIR=${CMDDIR}/tmp
+mkdir -p ${TMPDIR}
+TMPDIRS+=" $TMPDIR";
+
+if [ "$1" == "debug" ]; then
+    set -x
+fi
+
+# ---------------------------------------------------------------------------
+# Test 01: block decomposition of even-length blocks
+# ---------------------------------------------------------------------------
+
+echo "testing decompose_blocksub of even-length blocks"
+
+${VT} \
+    decompose_blocksub \
+    ${CMDDIR}/01_IN_even_length.vcf \
+    -o ${TMPDIR}/01_OUT_even_length.vcf \
+    2>&1 | strip_stderr > ${TMPDIR}/01_OUT_even_length.stderr
+
+OUT=`diff ${CMDDIR}/01_OUT_even_length.vcf ${TMPDIR}/01_OUT_even_length.vcf`
+ERR=`diff ${CMDDIR}/01_OUT_even_length.stderr ${TMPDIR}/01_OUT_even_length.stderr`
+
+set +x
+
+echo -n "             output VCF file :"
+if [ "$OUT" == "" ]; then
+    echo " ok"
+else
+    echo " NOT OK!!!"
+fi
+
+echo -n "             output logs     :"
+if [ "$ERR" == "" ]; then
+    echo " ok"
+else
+    echo " NOT OK!!!"
+fi
+
+# ---------------------------------------------------------------------------
+# Test 02: block decomposition with alignment
+# ---------------------------------------------------------------------------
+
+echo "testing decompose_blocksub with alignment"
+
+${VT} \
+    decompose_blocksub -a \
+    ${CMDDIR}/02_IN_uneven_length.vcf \
+    -o ${TMPDIR}/02_OUT_uneven_length.vcf \
+    2>&1 | strip_stderr > ${TMPDIR}/02_OUT_uneven_length.stderr \
+
+OUT=`diff ${CMDDIR}/02_OUT_uneven_length.vcf ${TMPDIR}/02_OUT_uneven_length.vcf`
+ERR=`diff ${CMDDIR}/02_OUT_uneven_length.stderr ${TMPDIR}/02_OUT_uneven_length.stderr`
+
+set +x
+
+echo -n "             output VCF file :"
+if [ "$OUT" == "" ]; then
+    echo " ok"
+else
+    echo " NOT OK!!!"
+fi
+
+echo -n "             output logs     :"
+if [ "$ERR" == "" ]; then
+    echo " ok"
+else
+    echo " NOT OK!!!"
+fi
+
+trap "rm -rf ${TMPDIRS}" EXIT KILL TERM INT HUP
