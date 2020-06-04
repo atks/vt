ab6c13cfdd014d77cd0f1975d92af3500fc8f71d
Adrian Tan <atks@umich.edu>
2020-06-04 08:29:50 -0400

    added str annotator for classic representation

---
+++ str_annotator.cpp
@@ -0,0 +1,153 @@
+/* The MIT License
+
+   Copyright (c) 2017 Adrian Tan <atks@umich.edu>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#include "indel_annotator.h"
+
+/**
+ * Constructor.
+ */
+IndelAnnotator::IndelAnnotator(std::string& ref_fasta_file, bool debug)
+{
+    vm = new VariantManip(ref_fasta_file.c_str());
+
+    fai = fai_load(ref_fasta_file.c_str());
+    if (fai==NULL)
+    {
+        fprintf(stderr, "[%s:%d %s] Cannot load genome index: %s\n", __FILE__, __LINE__, __FUNCTION__, ref_fasta_file.c_str());
+        exit(1);
+    }
+
+    cre = new CandidateRegionExtractor(ref_fasta_file, debug);
+    cmp = new CandidateMotifPicker(debug);
+    fd = new FlankDetector(ref_fasta_file, debug);
+
+    this->debug = debug;
+};
+
+/**
+ * Destructor.
+ */
+IndelAnnotator::~IndelAnnotator()
+{
+    delete vm;
+    fai_destroy(fai);
+}
+
+/**
+ * Annotates VNTR characteristics.
+ * @mode - e for exact alignment annotation
+ *       - f for fuzzy alignment annotation
+ *       -
+ */
+void IndelAnnotator::annotate(Variant& variant)
+{
+    VNTR& vntr = variant.vntr;
+
+    bcf_hdr_t* h = variant.h;
+    bcf1_t* v = variant.v;
+
+    //update chromosome and position
+    variant.rid = bcf_get_rid(v);
+    variant.pos1 = bcf_get_pos1(v);
+
+    //this is for reannotating an VNTR record
+    //this is more for the purpose of evaluation to
+    //check if vt's algorithm is concordant with
+    //VNTRs from other sources.
+    if (variant.type==VT_VNTR)
+    {
+        if (debug) std::cerr << "============================================\n";
+        if (debug) std::cerr << "ANNOTATING VNTR/STR \n";
+        
+        //1. pick candidate region
+        cre->pick_candidate_region(variant, REFERENCE, FINAL);
+
+        //2. set motifs from info field
+        cmp->set_motif_from_info_field(variant);
+
+        //3. compute purity scores
+        fd->compute_purity_score(variant, FINAL);
+
+        //4. compute composition and sequence statistics
+        fd->compute_composition_and_entropy(variant, FINAL);
+    }
+    //main purpose - annotation of Indels.
+    else if (variant.type&VT_INDEL)
+    {
+        //the basic steps in annotating a TR
+        //
+        //1. extract a region that has a chance of containing the repeat units
+        //2. choose a set of candidate motifs and pick motif
+        //3. detect repeat region and evaluate
+        //4. iterate 2 and 3
+        
+        if (debug) std::cerr << "============================================\n";
+        if (debug) std::cerr << "ANNOTATING INDEL\n";
+
+
+
+
+
+        //1. selects candidate region by fuzzy left and right alignment
+        cre->pick_candidate_region(variant, EXACT_LEFT_RIGHT_ALIGNMENT, EXACT);
+        cmp->update_exact_repeat_unit(variant);
+        
+        //2. detect candidate motifs from a reference sequence
+        cmp->generate_candidate_motifs(variant);
+
+        //this cannot possibly fail as next_motif() guarantees it
+        if (!cmp->next_motif(variant, CHECK_MOTIF_PRESENCE_IN_ALLELE))
+        {
+            //fall back on exact motif chosen.
+            VNTR& vntr = variant.vntr;
+            vntr.fuzzy_motif = vntr.exact_motif;
+            vntr.fuzzy_ru = vntr.exact_ru;
+            vntr.fuzzy_basis = vntr.exact_basis;
+            vntr.fuzzy_mlen = vntr.exact_mlen;
+            vntr.fuzzy_blen = vntr.exact_blen;;
+            
+            if (debug) std::cerr << "updating fuzzy motif with exact motifs\n";
+        }
+
+        fd->detect_flanks(variant, EXACT);
+        fd->compute_purity_score(variant, EXACT);
+        fd->compute_composition_and_entropy(variant, EXACT);
+
+        fd->detect_flanks(variant, FUZZY);
+        fd->compute_purity_score(variant, FUZZY);
+        fd->compute_composition_and_entropy(variant, FUZZY);
+
+        //introduce reiteration based on concordance and exact concordance.
+
+        if (debug)
+        {
+            std::cerr << "\n";
+            vntr.print();
+            std::cerr << "\n";
+        }
+
+        if (debug) std::cerr << "============================================\n";
+        return;
+        
+    }
+}
---
+++ str_annotator.h
@@ -0,0 +1,144 @@
+/* The MIT License
+
+   Copyright (c) 2017 Adrian Tan <atks@umich.edu>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#ifndef INDEL_ANNOTATOR_H
+#define INDEL_ANNOTATOR_H
+
+#include "candidate_region_extractor.h"
+#include "candidate_motif_picker.h"
+#include "flank_detector.h"
+
+#define START                          0
+#define EXACT_LEFT_AND_RIGHT_ALIGNMENT 1
+#define ANNOTATE_EXACT                 1
+#define PICK_CANDIDATE_REPEAT_UNITS    1
+#define FUZZY_LEFT_AND_RIGHT_ALIGNMENT 2
+#define EVALUATION                     3
+#define CHOOSE_FINAL_ANNOTATION        4
+#define ANNOTATE_FUZZY                 7
+#define END                            8
+
+/**
+ * Class for determining basic traits of an indel
+ * motifs, flanks and VNTR type statistics.
+ * MOTIF
+ * RU
+ * RL
+ * FLANKS
+ * FZ_FLANKS
+ * FZ_CONCORDANCE
+ *
+ * This is intended to replace VNTRAnnotator by implementing the
+ * annotation process as a finite state machine.
+ *
+1. START
+
+2. EXACT_LEFT_AND_RIGHT_ALIGNMENT
+
+3. ANNOTATE_EXACT
+
+4. PICK_CANDIDATE_REPEAT_UNITS
+
+5. FUZZY_LEFT_AND_RIGHT_ALIGNMENT
+
+6. EVALUATION
+   if fail requirements, go back to PICK_CANDIDATE_REPEAT_UNITS
+        a. fuzzy should be inclusive of exact
+        b. when fuzzy does not overlap exact
+           - retry with another repeat unit
+      redo till no acceptable solution
+   last choice is to use EX_RU
+
+7. CHOOSE_FINAL_ANNOTATION
+
+8. END
+
+ALGOTRACE tags
+ALGO_TRIES - number of iterations
+ALGO_EXACT - final answer is exact==fuzzy
+ALGO_RU_TRIES - repeat unit tries
+
+other useful tags
+EXNEFZRU - fuzzy repeat unit not the same as exact repat unit
+
+requirements: a. fuzzy should be inclusive of exact
+              b. when fuzzy does not overlap exact
+                  - retry with another repeat unit
+
+#other useful annotations = relationship between EX_RU and FZ_RU
+
+#consider consolidation program as a form of evaluation.
+
+ */
+class IndelAnnotator
+{
+    public:
+
+    /////////
+    //options
+    /////////
+    bool debug;
+    int32_t max_len;
+
+    ///////
+    //tools
+    ///////
+    VariantManip *vm;
+    faidx_t* fai;
+    CandidateRegionExtractor* cre;
+    CandidateMotifPicker* cmp;
+    FlankDetector* fd;
+
+    //for retrieving sequences
+    int8_t* seq;
+
+    //factors[n][index], for determining what sub repeat units to examine
+    int32_t** factors;
+
+    //finite state machine
+    int32_t state;
+
+    /**
+     * Constructor.
+     */
+    IndelAnnotator(std::string& ref_fasta_file, bool debug=false);
+
+    /**
+     * Destructor.
+     */
+    ~IndelAnnotator();
+
+    /**
+     * Annotates VNTR characteristics.
+     *
+     * @mode
+     *   e - determine by exact alignment
+     *   f - determine by fuzzy alignment
+     *   p - determine by penalized fuzzy alignment
+     *   h - using HMMs
+     *   x - integrated models
+     */
+    void annotate(Variant& variant);
+};
+
+#endif
